fa216c74dec6f510d98cf614b13cd8742e3e576c
ragnarok <62729064+elbissousa27@users.noreply.github.com>
2022-09-05 00:10:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const classAtivo = 'ativo'
+function initTab(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if(tabMenu.length && tabContent.length) {
+
+        tabContent[0].classList.add(classAtivo);
+
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove(classAtivo);
+            });
+            tabContent[index].classList.add(classAtivo);
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
+}
+initTab();
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    if(accordionList.length){
+
+        accordionList[0].classList.add(classAtivo);
+        accordionList[0].nextElementSibling.classList.add(classAtivo);
+
+        function activeAccordion(event){
+            this.classList.toggle(classAtivo);
+            this.nextElementSibling.classList.toggle(classAtivo);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion()
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+
+
+        // forma alternativa
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+
+initScrollSuave();
+
+
+
