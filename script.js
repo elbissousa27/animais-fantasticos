dce897cea5ccda2211db547878780557b82aecbc
ragnarok <62729064+elbissousa27@users.noreply.github.com>
2022-09-05 21:25:26 -0300

    comitt projet final

--- script.js
+++ script.js
@@ -71,3 +71,26 @@ function initScrollSuave(){
     });
 }
 initScrollSuave()
+
+function initanimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(section.length){
+        const windowMetade = window.innerHeight*0.6;
+
+        function animaScroll(){
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible){
+                    section.classList.add('ativo');
+                }
+            })
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+initanimacaoScroll();
